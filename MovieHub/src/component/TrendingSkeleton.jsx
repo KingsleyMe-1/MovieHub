5dac94985d8ecc94c03f9b9e80d7066b594f72fc
Kingsley <kjdigman132@gmail.com>
2026-02-07 02:05:20 +0800

    Added the skeleton loader for better UI/UX

---
+++ MovieHub/src/component/TrendingSkeleton.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "../styles/TrendingSkeleton.css"
+
+const TrendingSkeleton = ({ count = 5 }) => {
+    return (
+        <ul>
+            {Array.from({ length: count }).map((_, index) => (
+                <li key={`trending-skeleton-${index}`}>
+                    <p className="skeleton-number">{index + 1}</p>
+                    <div className="skeleton-trending-poster"></div>
+                </li>
+            ))}
+        </ul>
+    )
+}
+
+export {TrendingSkeleton};
+export default TrendingSkeleton;
