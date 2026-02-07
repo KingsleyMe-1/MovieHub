d2f58a18796cf171c543137cbeb1a99bbb432524
Kingsley <kjdigman132@gmail.com>
2026-02-07 19:43:21 +0800

    Fix deployment error

--- MovieHub/src/main.jsx
+++ MovieHub/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store } from './store/store.jsx';
+import { store } from './store/store.js';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import './index.css';
 import App from './App.jsx';
