f1c80827c89a6d6f2c27d0afe98f5abba17a3d3a
Kingsley <kjdigman132@gmail.com>
2026-02-07 20:01:03 +0800

    Fix the error mispelled

--- MovieHub/src/store/store.js
+++ MovieHub/src/store/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import moviesReducer from './moviesSlice';
+import movieReducer from './moviesSlice';
 
 export const store = configureStore({
   reducer: {
-    movies: moviesReducer,
+    movies: movieReducer,
   },
 });
