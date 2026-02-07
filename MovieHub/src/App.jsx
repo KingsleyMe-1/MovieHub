f153878b6e4c9f1bce4a1ab816b870a785f64703
Kingsley James Digman <kjdigman132@gmail.com>
2026-02-07 23:34:08 +0800

    Added the watchlist feature

--- MovieHub/src/App.jsx
+++ MovieHub/src/App.jsx
@@ -3,6 +3,7 @@ import { AuthProvider } from './context/AuthContext';
 import HomePage from './component/HomePage';
 import MovieDetails from './component/MovieDetails';
 import SignIn from './component/SignIn';
+import Watchlist from './component/Watchlist';
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
           <Route path='/' element={<HomePage />} />
           <Route path='/movie/:movieId' element={<MovieDetails />} />
           <Route path='/signin' element={<SignIn />} />
+          <Route path='/watchlist' element={<Watchlist />} />
         </Routes>
       </Router>
     </AuthProvider>
